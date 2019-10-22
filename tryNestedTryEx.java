13dcf254cd1fd6be7e2db9ac070411047acb63a9
vishalamc <vishalamc@gmail.com>
2019-10-22 12:18:18 +0530

    Add files via upload

---
+++ tryNestedTryEx.java
@@ -0,0 +1,29 @@
+class tryNestedTryEx
+{
+	public static void main(String []args)
+	{
+		int []num=new int[]{1,2,3};
+		
+		try
+		{
+		int res=10/5;
+		
+		System.out.println(res);
+			try{
+				System.out.println(num[5]);
+			   }
+			catch(ArrayIndexOutOfBoundsException  ex1)
+			{
+			ex1.printStackTrace();
+			}
+		}
+		catch(Exception  ex2)
+		{
+		ex2.printStackTrace();
+		}
+		
+		
+	
+	}
+
+}
